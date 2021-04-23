35b22dd945d36b84f1aac07dbc63348e4a727d21
Moumen Hallak <moumen95@gmail.com>
2021-04-23 22:33:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ function unleashDog(dogName, dogBreed) {
 function exerciseDog(dogName, dogBreed){
   let exercise = [];
   for (let i = 0; i < routine.length ; i++){
-  exercise.push(routine[i](dogName, dogBreed));
 }
   return (exercise);
 }
