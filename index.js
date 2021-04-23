f5e834999a44d67de52e6e4145203026dc32d2ea
Moumen Hallak <moumen95@gmail.com>
2021-04-23 21:54:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-  const dogName = "Xerox, Boo Radley, Bunny, Mary J. Blige";
+  const dogName = "Xerox, Boo Radley, Bunny, Mary J. Blige, Santa Paws";
   const dogBreed = "Border Collie";
 function wakeDog(dogName, dogBreed) {
   return("Wake " + dogName[0] + " the " + dogBreed[0]);
