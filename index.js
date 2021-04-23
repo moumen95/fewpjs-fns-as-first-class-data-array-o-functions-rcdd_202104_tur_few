0aeb3e4b994e377770ab138a10eddcf2707668db
Moumen Hallak <moumen95@gmail.com>
2021-04-23 22:25:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,42 +1,28 @@
 let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
 
 function wakeDog(dogName, dogBreed) {
-  // const dogName = ['Xerox'];
-  // const dogBreed = ['Border Collie'];
-
   return("Wake " + dogName + " the " + dogBreed);
 }
 function leashDog(dogName, dogBreed) {
-  // const dogName = ['Boo Radley'];
-  // const dogBreed = ['Pibble'];
-
   return("Leash " + dogName + " the " + dogBreed);
 }
 
 
 function walkToPark(dogName, dogBreed) {
-  // const dogName = ['Bunny'];
-  // const dogBreed = ['Labrador'];
   return("Walk to the park with " + dogName + " the " + dogBreed);
 }
 
 function throwFrisbee(dogName, dogBreed) {
-  // const dogName = ['Mary J. Blige'];
-  // const dogBreed = ['Papillon'];
   return("Throw the frisbee for " + dogName + " the " + dogBreed);
 }
 
 function walkHome(dogName, dogBreed) {
-  // const dogName = ['Santa Paws'];
-  // const dogBreed = ['Husky'];
   return("Walk home with " + dogName + " the " + dogBreed);
 }
 
 
 function unleashDog(dogName, dogBreed) {
-// const dogName = ['Lauren'];
-// const dogBreed = ['Golden Retriever'];
   return("Unleash " + dogName + " the " + dogBreed);
 }
 
-
+function exerciesDog(dogName, dogBreed);
